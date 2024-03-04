07062e896d7ce3e7c17d1f006802ece0773c2f56
Roberto_Graziadei <roberto.2022315930@aluno.iffar.edu.br>
2024-03-04 17:13:06 -0300

    Criando o crud

---
+++ conecta.php
@@ -0,0 +1,10 @@
+<?php
+
+$servidor = "localhost";
+$ususario = "root";
+$senha = "";
+$banco = "git";
+
+$conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+?>
--- index.php
+++ index.php
@@ -1,11 +1,28 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página inicial</title>
+    <title>Crud</title>
 </head>
 <body>
-    Olá mundo!
+    <table>
+        <thead>
+            <th>Id</th>
+            <th>Email</th>
+            <th colspan="2">Opções</th>
+        </thead>
+        <tbody>
+        <th>
+        <td>1</td>
+        <td>Roberto</td>
+        <td>Alterar</td>
+        <td>Excluir</td>
+        </tr>
+        </tbody>
+
+    </table>
 </body>
 </html>
+
